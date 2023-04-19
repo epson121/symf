224e9d3073a4997238ebb965aee51a7f846a2d66
Luka Rajcevic <traxdater12@gmail.com>
2023-04-19 16:23:36 +0200

    Show all lyrics

--- guestbook/bundles/CustomBundle/src/Controller/LyricsController.php
+++ guestbook/bundles/CustomBundle/src/Controller/LyricsController.php
@@ -2,17 +2,23 @@
 
 namespace CustomBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use CustomBundle\Entity\Lyrics;
+use Doctrine\ORM\EntityManagerInterface;
+use CustomBundle\Repository\LyricsRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LyricsController extends AbstractController
 {
     #[Route('/lyrics', name: 'app_lyrics')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {        
+
+        $lyrics = $entityManager->getRepository(Lyrics::class)->findAll();
+
         return $this->render('lyrics/index.html.twig', [
-            'controller_name' => 'LyricsController',
+            'lyrics' => $lyrics
         ]);
     }
 }
